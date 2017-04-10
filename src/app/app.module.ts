736f17ff9652d6044495589f74437e7516efb9e9
alan.truong <atruongcontact@gmail.com>
2017-04-10 22:34:48 +0000

    added benchy test image. added test footer to the side-nav for future social links

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { RouterModule } from '@angular/router';
 import { AlertModule } from 'ng2-bootstrap';
 import { PrintsModule } from './prints/prints.module';
 
 import { Auth } from './common/services/auth0/auth0.service'
 
 import { AppComponent } from './app.component';
-import { RoutingModule } from './routing/routing.module';
+import { RoutingModule} from './routing/routing.module';
 import { HomeComponent } from './home/home.component';
 import { BrandBarComponent } from './core/brand-bar/brand-bar.component';
 
@@ -25,7 +24,6 @@ import { BrandBarComponent } from './core/brand-bar/brand-bar.component';
     FormsModule,
     HttpModule,
     RoutingModule,
-    RouterModule,
     AlertModule.forRoot()
   ],
   providers: [Auth],
